b5ef38f32392e43947ab6ed79f2fea20cdc68111
NiltonCortesJr <niltoncjr@gmail.com>
2022-02-08 23:25:04 -0300

    Atualizando as atividades do dia 07 de fevereiro de 2022

--- src/front/models/anotacoes.js
+++ src/front/models/anotacoes.js
@@ -6,11 +6,15 @@ class Anotacoes {
 
         if(typeof this.titulos != "string"){
             console.log("Criação de título - Dados inválidos");
+        } else{
+            console.log("Títulos OK");
         }
 
         if(typeof this.notas != "string"){
             console.log("Criação de notas - Dados inválidos");
+        } else{
+            console.log("Notas OK");
         }
     }
 }
-const teste = new Anotacoes(10, "estudante");
+// const teste = new Anotacoes(10, "estudante");
